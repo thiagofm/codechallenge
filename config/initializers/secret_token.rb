d93a869d6e39567a3f73b06e7b0017e127fdd41a
Thiago Fernandes Massa <thiagown@gmail.com>
2014-10-04 14:59:39 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codechallenge::Application.config.secret_token = '763641211f6656d390073ab2f17030763724fdbb0a2e07f22945db6d09b62e558838e925350f72cfa6d4b7aa43a8966e768955d834d8c715f979663ad8b6d2f3'
