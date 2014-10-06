c6dc029a653fd554143602fe2f4ff2b7baf9839d
Thiago Fernandes Massa <thiagown@gmail.com>
2014-10-05 23:31:45 -0300

    Finished up controllers

--- app/controllers/paths_controller.rb
+++ app/controllers/paths_controller.rb
@@ -1,7 +1,27 @@
 class PathsController < ApplicationController
+  before_filter :validate_params
+  before_filter :load_station
+
   def shortest
+    render status: 200, json: {
+      shortest: Station.route_between(@station_1, @station_2)
+                       .map {|route| route['name'] }
+                       .select{|name| true if name.present? }
+    }.to_json
   end
 
   def approximate_time
+    render status: 200, json: { approximate_time_in_minutes: Station.approximate_time(@station_1, @station_2).to_json }
+  end
+
+  private
+  def load_station
+    @station_1 = Station.find_by_number(params[:station_1])
+    @station_2 = Station.find_by_number(params[:station_2])
+    render status: 400 unless @station_1.present? && @station_2.present?
+  end
+
+  def validate_params
+    render status: 400 unless params.include?(:station_1) && params.include?(:station_2)
   end
 end
--- spec/controllers/paths_controller_spec.rb
+++ spec/controllers/paths_controller_spec.rb
@@ -1,18 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe PathsController, :type => :controller do
+  after(:all) do
+    Neo4j.shutdown
+    `rm -rf db/neo4j-test/`
+  end
+
+  before(:all) do
+    Station.create_stations
+    Station.create_routes
+    Line.create_lines
+  end
+
   describe "GET shortest" do
-    it "returns http success" do
+    it "returns bad request when the parameters aren't specified" do
       get :shortest
+      expect(response).to have_http_status(:bad_request)
+    end
+
+    it "returns bad request when one of the stations aren't found" do
+      get :shortest, { station_1: '11', station_2: '9999' }
+      expect(response).to have_http_status(:bad_request)
+    end
+
+    it 'discovers successfully the shortest path' do
+      get :shortest, { station_1: '11', station_2: '290' }
       expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body)).to eq({"shortest"=>["Baker Street", "Finchley Road", "West Hampstead"]})
     end
   end
 
   describe "GET approximate_time" do
-    it "returns http success" do
+    it "returns bad request when the parameters aren't specified" do
       get :approximate_time
+      expect(response).to have_http_status(:bad_request)
+    end
+
+    it "returns bad request when one of the stations aren't found" do
+      get :approximate_time, { station_1: '11', station_2: '9999' }
+      expect(response).to have_http_status(:bad_request)
+    end
+
+    it 'discovers successfully the shortest path' do
+      get :approximate_time, { station_1: '11', station_2: '290' }
       expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body)).to eq({"approximate_time_in_minutes"=>"15"})
     end
   end
-
 end
